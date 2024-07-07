0e677c02ae8127b0cb44cc35f8cbde443a11c9ab
marlon-erler <viridian035@gmail.com>
2024-07-07 16:51:26 +0200

    fix service worker bug

--- dist/sw.js
+++ dist/sw.js
@@ -1,31 +1,38 @@
-// TABS
-document.querySelector("main").addEventListener("scroll", () => {
-  const links = document.querySelectorAll(".tab-link");
-  links.forEach((element) => element.removeAttribute("active"));
+const CACHE_VERSION = "v1";
 
-  const pageWidth = document.querySelector("main > article").offsetWidth;
-  const scroll = document.querySelector("main").scrollLeft;
-  const currentPage = Math.round(scroll / pageWidth);
+async function handleRequest(event) {
+  const responsePromise = getResponse(event);
+  event.respondWith(responsePromise);
+}
 
-  if (links[currentPage]) links[currentPage].setAttribute("active", "");
-});
-
-// SERVICE WORKER
-async function registerServiceWorker() {
-  if ("serviceWorker" in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register("/sw.js", {
-        scope: "/",
-      });
-      if (registration.installing) {
-        setTimeout(() => window.location.reload(), 500);
-      } else if (registration.active) {
-        console.log("Service worker active");
-      }
-    } catch (error) {
-      console.error(`Could not install service worker: ${error}`);
-    }
+async function getResponse(event) {
+  try {
+    return await fetchAndCache(event);
+  } catch {
+    return await getFromCache(event);
   }
 }
 
-registerServiceWorker();
+async function fetchAndCache(event) {
+  const request = event.request;
+
+  const response = await fetch(request);
+
+  const cache = await caches.open(CACHE_VERSION);
+  await cache.put(request, response.clone());
+
+  return response;
+}
+
+async function getFromCache(event) {
+  const request = event.request;
+
+  const cache = await caches.open(CACHE_VERSION);
+  const response = await cache.match(request);
+
+  return response;
+}
+
+self.addEventListener("fetch", (event) => {
+  handleRequest(event);
+});
